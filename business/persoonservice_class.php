e0d898dd3d21ae84e550f2d5febf64b4664b5578
lok117 <lok117@LOK117-01>
2014-08-25 12:29:54 +0200

    hell

---
+++ business/persoonservice_class.php
@@ -0,0 +1,9 @@
+<?php
+require_once("data/persoonDAO.php");
+class PersoonService {
+    public function getPersonenOverzicht() {
+        $pDAO = new PersoonDAO();
+        $personen = $pDAO->getAll();
+        return $personen;
+    }
+}
